c46032cbd9c473be3f68baf88d10dd5972961b8d
Marcelo Matos <marcelomatos.dev@gmail.com>
2019-11-20 18:15:55 +0000

    Home page style complete

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,7 +1,37 @@
-import React from 'react';
+// import React from 'react';
 
-// import { Container } from './styles';
+// // import { Container } from './styles';
 
-export default function Cart() {
-   return <h1>Cart</h1>;
-}
+// export default function Cart() {
+//    return (
+//    <Container>
+//       <ProductTable>
+//          <thead>
+//             <tr>
+//                <th/>
+//                <th>PRODUCT</th>
+//                <th>QUANTITY</th>
+//                <th>SUBTOTAL</th>
+//                <th/>
+//             </tr>
+//          </thead>
+//          <tbody>
+//          <tr>
+//             <td>
+//                <
+//             </td>
+//          </tr>
+//          </tbody>
+
+//       </ProductTable>
+//       <footer>
+//          <button type="button">Finish order</button>
+//          <Total>
+//             <span>TOTAL</span>
+//             <strong>R$1900.52</strong>
+//          </Total>
+//       </footer>
+//    </Container>
+
+//       );
+// }
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,89 @@
 import React from 'react';
+import {MdAddShoppingCart} from 'react-icons/md';
 
-// import { Container } from './styles';
+import {ProductList} from './styles';
 
-export default function Home() {
-   return <h1>Home</h1>;
+export default function Home(){
+   return(
+   <ProductList>
+      <li>
+         <img src="https://images.footlocker.com/is/image/EBFL2/4523455_a1?wid=640&hei=640&fmt=png-alpha" alt="Vans" />
+         <strong>Nice shoes</strong>
+         <span>CAD 99.88</span>
+
+         <button type="button">
+            <div>
+               <MdAddShoppingCart size={16} color="#FFF" /> 3
+            </div>
+
+            <span>ADD TO CART</span>
+         </button>
+      </li>
+      <li>
+         <img src="https://images.footlocker.com/is/image/EBFL2/4523455_a1?wid=640&hei=640&fmt=png-alpha" alt="Vans" />
+         <strong>Nice shoes</strong>
+         <span>CAD 99.88</span>
+
+         <button type="button">
+            <div>
+               <MdAddShoppingCart size={16} color="#FFF" /> 3
+            </div>
+
+            <span>ADD TO CART</span>
+         </button>
+      </li>
+      <li>
+         <img src="https://images.footlocker.com/is/image/EBFL2/4523455_a1?wid=640&hei=640&fmt=png-alpha" alt="Vans" />
+         <strong>Nice shoes</strong>
+         <span>CAD 99.88</span>
+
+         <button type="button">
+            <div>
+               <MdAddShoppingCart size={16} color="#FFF" /> 3
+            </div>
+
+            <span>ADD TO CART</span>
+         </button>
+      </li>
+      <li>
+         <img src="https://images.footlocker.com/is/image/EBFL2/4523455_a1?wid=640&hei=640&fmt=png-alpha" alt="Vans" />
+         <strong>Nice shoes</strong>
+         <span>CAD 99.88</span>
+
+         <button type="button">
+            <div>
+               <MdAddShoppingCart size={16} color="#FFF" /> 3
+            </div>
+
+            <span>ADD TO CART</span>
+         </button>
+      </li>
+      <li>
+         <img src="https://images.footlocker.com/is/image/EBFL2/4523455_a1?wid=640&hei=640&fmt=png-alpha" alt="Vans" />
+         <strong>Nice shoes</strong>
+         <span>CAD 99.88</span>
+
+         <button type="button">
+            <div>
+               <MdAddShoppingCart size={16} color="#FFF" /> 3
+            </div>
+
+            <span>ADD TO CART</span>
+         </button>
+      </li>
+      <li>
+         <img src="https://images.footlocker.com/is/image/EBFL2/4523455_a1?wid=640&hei=640&fmt=png-alpha" alt="Vans" />
+         <strong>Nice shoes</strong>
+         <span>CAD 99.88</span>
+
+         <button type="button">
+            <div>
+               <MdAddShoppingCart size={16} color="#FFF" /> 3
+            </div>
+
+            <span>ADD TO CART</span>
+         </button>
+      </li>
+   </ProductList>
+   );
 }
